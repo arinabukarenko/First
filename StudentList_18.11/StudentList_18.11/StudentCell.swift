6441dac6e214a2854b806981c0c767ccfe5d1d2d
arinabukarenko <arina.odessova@yandex.ru>
2021-11-18 22:49:49 +0300

    Student List (1st commit)

---
+++ StudentList_18.11/StudentList_18.11/StudentCell.swift
@@ -0,0 +1,14 @@
+//
+//  StudentCell.swift
+//  StudentList_18.11
+//
+//  Created by Арина  on 18.11.21.
+//
+
+import UIKit
+
+class StudentCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
