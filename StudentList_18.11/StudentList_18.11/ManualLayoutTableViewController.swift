e51775db8bd580551d63c397da7915c764c9df3b
arinabukarenko <arina.odessova@yandex.ru>
2021-11-21 09:54:59 +0300

    Add manual autolayout

---
+++ StudentList_18.11/StudentList_18.11/ManualLayoutTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ManualLayoutTableViewController.swift
+//  StudentList_18.11
+//
+//  Created by Арина  on 21.11.21.
+//
+
+import UIKit
+
+class ManualLayoutTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
