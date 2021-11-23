f473abb8e74ef04229c10f7f5bc0df3c5493d66b
arinabukarenko <arina.odessova@yandex.ru>
2021-11-23 17:41:01 +0300

    add "searchBar"

--- StudentList_18.11/StudentList_18.11/StudentListManual/ManualLayoutTableViewController.swift
+++ StudentList_18.11/StudentList_18.11/StudentListManual/ManualLayoutTableViewController.swift
@@ -7,9 +7,17 @@
 
 import UIKit
 
-class ManualLayoutTableViewController: UIViewController, UITableViewDelegate {
+class ManualLayoutTableViewController: UIViewController, UITableViewDelegate, UISearchBarDelegate, UISearchResultsUpdating{
+    func updateSearchResults(for searchController: UISearchController) {
+        
+    }
+    
     
     let tableView = UITableView()
+    private var searchController = UISearchController()
+    var filteresNames: [String]!
+    
+    
     
     var male = ["Aртимович Игорь Владимирович",
                 "Богданович Дмитрий Александрович",
@@ -30,10 +38,23 @@ class ManualLayoutTableViewController: UIViewController, UITableViewDelegate {
                   "Сандова Галина Александровна",
                   "Елисеева Марина Михайловна"].sorted()
     
-    //lazy var students2:[String] = male + female
+    lazy var students: [String] = male + female
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        filteresNames = students
+        
+        searchController = UISearchController(searchResultsController: nil)
+        tableView.tableHeaderView = searchController.searchBar
+        searchController.searchBar.delegate = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.placeholder = "Search"
+        
+        searchController.hidesNavigationBarDuringPresentation = false
+        
+        searchController.searchBar.tintColor = UIColor.black
         view.backgroundColor = .white
         
         tableView.register(NewStudentCell.self, forCellReuseIdentifier: "NewStudentCell")
@@ -46,6 +67,7 @@ class ManualLayoutTableViewController: UIViewController, UITableViewDelegate {
         tableView.delegate = self
         
         view.addSubview(tableView)
+        
         tableView.translatesAutoresizingMaskIntoConstraints = false
         
         tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
@@ -53,34 +75,53 @@ class ManualLayoutTableViewController: UIViewController, UITableViewDelegate {
         tableView.leadingAnchor.constraint(equalTo:view.leadingAnchor).isActive = true
         tableView.trailingAnchor.constraint(equalTo:view.trailingAnchor).isActive = true
         
-
     }
     
 }
-// MARK: - UITableViewDataSource
+// MARK: - Extension
 
 extension ManualLayoutTableViewController: UITableViewDataSource{
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
-    }
+        return 2}
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if section == 1{
-            return female.count
+            return filteresNames.count
         }
-            return male.count
-        }
+        return filteresNames.count}
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         if section == 1{
             return "Женщины \(female.count)"
         }
-            return "Мужчины \(male.count)"
-        }
+        return "Мужчины \(male.count)"
+    }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "NewStudentCell", for: indexPath) as! NewStudentCell
-        cell.titleLabel.text = male[indexPath.row]
+        cell.titleLabel.text = filteresNames[indexPath.row]
         if indexPath.section == 1{
-        cell.titleLabel.text = female[indexPath.row]
+            cell.titleLabel.text = filteresNames[indexPath.row]
+            
+        }
+        return cell
     }
-            return cell
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        if searchController.isActive {
+            return false
+        } else {
+            return true
         }
     }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteresNames = []
+        if searchText == "" {
+            filteresNames = students
+        }else{
+            for students1 in students {
+                if students1.contains(searchText){
+                    filteresNames.append(students1)
+                }
+            }
+        }
+        self.tableView.reloadData()
+    }
+}
